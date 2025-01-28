39797a40fab0bab74b6e81b499e5806134f32275
김윤혁 <kyhyeok@gmail.com>
2025-01-28 22:51:11 +0900

    Data Fetching
    Server Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,18 @@
-"use client"
+export const metadata =  {
+    title: "Home",
+}
 
-import {useEffect, useState} from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page () {
-    const [isLoading, setIsLoading] = useState(true );
-    const [movies, setMovies] = useState([] );
-    const getMovies = async () => {
-        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-        const json = await response.json();
-        setMovies(json);
-    }
-     useEffect(() => {
-        getMovies();
-        setIsLoading(false);
-     }, []);
+async function getMovies() {
+    return await fetch(URL).then(response => response.json());
+}
+
+export default async function HomePage () {
+    const movies = await getMovies();
     return (
         <div>
-            {isLoading ? "isLoading..." : JSON.stringify(movies)}
+            {JSON.stringify(movies)}
         </div>
     )
 }
