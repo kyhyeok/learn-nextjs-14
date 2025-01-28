8c416a0e0597b887b253e63c0b4dcd96dfe4153c
김윤혁 <kyhyeok@gmail.com>
2025-01-28 22:34:04 +0900

    Data Fetching
    Client Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,22 @@
-export const metadata = {
-    title: 'Home | Next Movies',
-}
+"use client"
+
+import {useEffect, useState} from "react";
 
 export default function Page () {
+    const [isLoading, setIsLoading] = useState(true );
+    const [movies, setMovies] = useState([] );
+    const getMovies = async () => {
+        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+        const json = await response.json();
+        setMovies(json);
+    }
+     useEffect(() => {
+        getMovies();
+        setIsLoading(false);
+     }, []);
     return (
         <div>
-            <h1>Hello NextJs!</h1>
+            {isLoading ? "isLoading..." : JSON.stringify(movies)}
         </div>
     )
 }
