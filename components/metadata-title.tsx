8ae32e5354e1fb2ab1f3d612c13a4d6887c8e697
김윤혁 <kyhyeok@gmail.com>
2025-01-31 22:57:18 +0900

    Deployment
    fixed Compile error,
    package.json fixed for vercel

--- components/metadata-title.tsx
+++ components/metadata-title.tsx
@@ -1,12 +1,14 @@
 import {getMovie} from "./movie-info";
 
-interface IParams {
-    params: {id: string};
-}
+type IParams = Promise<{
+    id: string;
+}>;
 
-export async function generateMetadata({params:{id}}: IParams) {
+export async function generateMetadata(props: { params: IParams }) {
+    const params = await props.params;
+    const id = params.id;
     const movie = await getMovie(id);
     return {
         title: movie.title,
-    }
+    };
 }
